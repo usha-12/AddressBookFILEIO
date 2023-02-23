708b48f7c5f90604fa1cf5417b279029ac66daf4
usha-12 <usha12ahirwar@gmail.com>
2023-02-23 21:47:24 +0530

    Added: Ability to add multiple person to Address Book

--- src/com/brideglabz/addressbookfileio/AddressBookMain.java
+++ src/com/brideglabz/addressbookfileio/AddressBookMain.java
@@ -34,57 +34,58 @@ public class AddressBookMain {
         contacts.add(contact);
     }
     public void editContacts() {
-        if(contacts.get(0).getF_name().equalsIgnoreCase(searchByName)) {
-            System.out.println("Enter respectively\n1.First name 2.Last name 3.Address" +
-                    " 4.City 5.State 6.Zip 7.Phone number 8.Email address");
-            int option2 = sc.nextInt();
-            switch (option2) {
-                case 1:
-                    System.out.println("Enter the first name to be edited");
-                    contacts.get(0).setF_name(sc.next());
-                    break;
-                case 2:
-                    System.out.println("Enter the last name to be edited");
-                    contacts.get(0).setL_name(sc.next());
-                    break;
-                case 3:
-                    System.out.println("Enter the address to be edited");
-                    contacts.get(0).setAddress(sc.next());
-                    break;
-                case 4:
-                    System.out.println("Enter the city name to be edited");
-                    contacts.get(0).setCity(sc.next());
-                    break;
-                case 5:
-                    System.out.println("Enter the state name to be edited");
-                    contacts.get(0).setState(sc.next());
-                    break;
-                case 6:
-                    System.out.println("Enter the zip code to be edited");
-                    contacts.get(0).setZip(sc.next());
-                    break;
-                case 7:
-                    System.out.println("Enter the phone number to be edited");
-                    contacts.get(0).setPh_no(sc.next());
-                    break;
-                case 8:
-                    System.out.println("Enter the email address to be edited");
-                    contacts.get(0).setEmail(sc.next());
-                    break;
-                default:
-                    System.out.println("Invalid option");
+        for(int i = 0; i < contacts.size(); i++) {
+            if(contacts.get(i).getF_name().equalsIgnoreCase(searchByName)) {
+                System.out.println("Enter respectively\n1.First name 2.Last name 3.Address" +
+                        " 4.City 5.State 6.Zip 7.Phone number 8.Email address");
+                int option2 = sc.nextInt();
+                switch (option2) {
+                    case 1:
+                        System.out.println("Enter the first name to be edited");
+                        contacts.get(i).setF_name(sc.next());
+                        break;
+                    case 2:
+                        System.out.println("Enter the last name to be edited");
+                        contacts.get(i).setL_name(sc.next());
+                        break;
+                    case 3:
+                        System.out.println("Enter the address to be edited");
+                        contacts.get(i).setAddress(sc.next());
+                        break;
+                    case 4:
+                        System.out.println("Enter the city name to be edited");
+                        contacts.get(i).setCity(sc.next());
+                        break;
+                    case 5:
+                        System.out.println("Enter the state name to be edited");
+                        contacts.get(i).setState(sc.next());
+                        break;
+                    case 6:
+                        System.out.println("Enter the zip code to be edited");
+                        contacts.get(i).setZip(sc.next());
+                        break;
+                    case 7:
+                        System.out.println("Enter the phone number to be edited");
+                        contacts.get(i).setPh_no(sc.next());
+                        break;
+                    case 8:
+                        System.out.println("Enter the email address to be edited");
+                        contacts.get(i).setEmail(sc.next());
+                        break;
+                    default:
+                        System.out.println("Invalid option");
+                }
             }
         }
-        else
-            System.out.println("Oops! can't find the name");
+
     }
     public void deleteContact() {
-        if(contacts.get(0).getF_name().equalsIgnoreCase(searchByName)) {
-            contacts.remove(0);
-            System.out.println("Deleted successfully"+contacts.size());
+        for(int i = 0; i < contacts.size(); i++) {
+            if(contacts.get(i).getF_name().equalsIgnoreCase(searchByName)) {
+                contacts.remove(i);
+                System.out.println("Deleted successfully"+contacts.size());
+            }
         }
-        else
-            System.out.println("Oops! can't find the name");
     }
     public void menu() {
         System.out.println("Menu: (Enter the respective number)\n1. Add contact 2. Edit contact 3. Delete 4. Display 5.Exit");
@@ -98,7 +99,7 @@ public class AddressBookMain {
             case 2:
                 System.out.println("Enter the first name to search and edit contact with first name");
                 searchByName = sc.next();
-                editContacts();
+                addressBook.editContacts();
                 addressBook.menu();
                 break;
             case 3:
@@ -107,13 +108,13 @@ public class AddressBookMain {
                 addressBook.deleteContact();
                 addressBook.menu();
             case 4:
-                if(contacts.size() > 0) {
-                    System.out.println(contacts.get(0));
-                    addressBook.menu();
-                }
-                else {
-                    System.out.println("Contact list in the addressbook is empty");
-                    addressBook.menu();
+                System.out.println("Enter the contact's first name to display");
+                searchByName = sc.next();
+                for(int i = 0; i < contacts.size(); i++) {
+                    if(contacts.get(i).getF_name().equalsIgnoreCase(searchByName)) {
+                        System.out.println(contacts.get(i));
+                        addressBook.menu();
+                    }
                 }
                 break;
             case 5:
