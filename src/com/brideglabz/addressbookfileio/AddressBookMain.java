f9d34b35ca6691e55f9fff8910ea1aebb27ccc08
usha-12 <usha12ahirwar@gmail.com>
2023-02-23 21:28:50 +0530

    Added: Ability to create a Contacts in Address book

--- src/com/brideglabz/addressbookfileio/AddressBookMain.java
+++ src/com/brideglabz/addressbookfileio/AddressBookMain.java
@@ -1,2 +1,38 @@
-package com.brideglabz.addressbookfileio;public class AddressBookMain {
+package com.brideglabz.addressbookfileio;
+
+import com.sun.tools.javac.Main;
+
+import java.util.Scanner;
+
+public class AddressBookMain {
+    static Contacts contact;
+    public void CreatingContact() {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the contact details");
+        System.out.println("Enter the first name");
+        String f_name = sc.next();
+        System.out.println("Enter the last name");
+        String l_name = sc.next();
+        System.out.println("Enter the address");
+        String address  = sc.next();
+        System.out.println("Enter the city name");
+        String city  = sc.next();
+        System.out.println("Enter the state name");
+        String state = sc.next();
+        System.out.println("Enter the Zip code");
+        String zip = sc.next();
+        System.out.println("Enter the phone number");
+        String ph_no = sc.next();
+        System.out.println("Enter the email address");
+        String email = sc.next();
+        System.out.println("Contact created");
+        contact = new Contacts(f_name,l_name,address,city,state,zip,ph_no,email);
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Welcome to the address book program");
+        AddressBookMain addressBook = new AddressBookMain();
+        addressBook.CreatingContact();
+        System.out.println(contact);
+    }
 }
--- src/com/brideglabz/addressbookfileio/Contacts.java
+++ src/com/brideglabz/addressbookfileio/Contacts.java
@@ -1,2 +1,95 @@
-package com.brideglabz.addressbookfileio;public class Contacts {
+package com.brideglabz.addressbookfileio;
+
+public class Contacts {
+    private String f_name; //first name
+    private String l_name; //last name
+    private String address;
+    private String city;
+    private String state;
+    private String zip;
+    private String ph_no; //phone number
+    private String email;
+
+    public Contacts() {
+    }
+
+    public String getF_name() {
+        return f_name;
+    }
+
+    public void setF_name(String f_name) {
+        this.f_name = f_name;
+    }
+
+    public String getL_name() {
+        return l_name;
+    }
+
+    public void setL_name(String l_name) {
+        this.l_name = l_name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public String getZip() {
+        return zip;
+    }
+
+    public void setZip(String zip) {
+        this.zip = zip;
+    }
+
+    public String getPh_no() {
+        return ph_no;
+    }
+
+    public void setPh_no(String ph_no) {
+        this.ph_no = ph_no;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+    public Contacts(String f_name,String l_name,String address,String city,String state,String zip,String ph_no,String email) {
+        this.f_name = f_name;
+        this.l_name = l_name;
+        this.address = address;
+        this.city = city;
+        this.state = state;
+        this.zip = zip;
+        this.ph_no = ph_no;
+        this.email = email;
+    }
+    public String toString() {
+        return "\nFirst name: "+f_name+"\nLast name: "+l_name
+                +"\nAddress: "+address+"\nCity: "+city
+                +"\nState: "+state+"\nZip: "+zip
+                +"\nPhone number: "+ph_no+"\nEmail address: "+email;
+    }
 }
